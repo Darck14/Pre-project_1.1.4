ee6ac94f34dd29df11f231c415c656f1afa24310
Dmitriy Bogatov <darck538567@gmail.com>
2024-05-27 18:00:42 +0400

    created a database connection

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,22 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private final static String URL = "jdbc:mysql://localhost:3306/database_test";
+    private final static String USERNAME = "Darck5385";
+    private final static String PASSWORD = "";
+
+    public static Connection getConnection() {
+        Connection conn = null;
+
+        try {
+            conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
 }
